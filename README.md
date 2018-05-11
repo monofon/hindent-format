8117fab47471e124ea23a9d7abb43df17c719caa
monofon <monofon@tramberend.de>
2018-05-11 16:28:30 +0200

    Some minimal maintenance for the latest vscode

--- README.md
+++ README.md
@@ -1,33 +1,65 @@
-# Hindent Format
+# hindent-format README
 
-A Visual Studio Code formatter for Haskell using [hindent](https://github.com/chrisdone/hindent).
+This is the README for your extension "hindent-format". After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
--   Supports document and document range formatting.
+Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+
+For example if there is an image subfolder under your extension project workspace:
+
+\!\[feature X\]\(images/feature-x.png\)
+
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
 
 ## Requirements
 
-Requires [hindent](https://github.com/chrisdone/hindent) to be installed.
+If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
 
 ## Release Notes
 
-### 0.0.5
+Users appreciate release notes as you update your extension.
 
--   Fix range specification for document formatting
+### 1.0.0
 
-### 0.0.4
+Initial release of ...
 
--   Fix hindent command line option spelling (fixes \#1).
+### 1.0.1
 
-### 0.0.3
+Fixed issue #.
 
--   Use `editor.wrappingColumn` if no line width is given on the hindent commandline.
+### 1.1.0
 
-### 0.0.2
+Added features X, Y, and Z.
 
--   Removed directory from the hindent default command path.
+-----------------------------------------------------------------------------------------------------------
 
-### 0.0.1
+## Working with Markdown
 
--   Initial release. Developed and tested on macOS.
+**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+
+* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
+* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
+* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+
+### For more information
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,11 @@ import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 
 class HindentFormatEditsProvider implements
-    vscode.DocumentFormattingEditProvider,
-    vscode.DocumentRangeFormattingEditProvider {
-  command: string = null;
-  arguments: Array<string> = null;
+  vscode.DocumentFormattingEditProvider,
+  vscode.DocumentRangeFormattingEditProvider {
+  hindentFound: boolean = false;
+  command: string = "hindent";
+  arguments: Array<string> = [];
 
   constructor() { this.configure(); }
 
@@ -18,33 +19,31 @@ class HindentFormatEditsProvider implements
 
     let result = child_process.spawnSync(args[0], ['--version']);
     if (!result.status) {
+      this.hindentFound = true;
       this.command = args[0];
       this.arguments = args.slice(1);
 
       // Use `editor.wrappingColumn` if no line width is given on the hindent
       // commandline.
-      if (this.arguments.indexOf('--line-length') == -1) {
+      if (this.arguments.indexOf('--line-length') === -1) {
         const wrappingColumn = vscode.workspace.getConfiguration('editor').get(
-            'wrappingColumn', '80');
+          'wrappingColumn', '80');
         this.arguments.push('--line-length');
         this.arguments.push(wrappingColumn);
       }
-    } else {
-      this.command = null;
-      this.arguments = null;
     }
   }
 
   formatHindent(text: string) {
-    if (this.command != null) {
+    if (this.hindentFound) {
       let result = child_process.spawnSync(
-          this.command, this.arguments, {'input': text});
+        this.command, this.arguments, { 'input': text });
       if (!result.status) {
         return result.stdout.toString();
       } else {
         // TODO Make warning widget disappear after 5 seconds.
-        vscode.window.showWarningMessage(result.stderr.toString());
-        return text;
+        vscode.window.showWarningMessage(result.stderr.toString().split('\n')[0]);
+        return '';
       }
     } else {
       return '';
@@ -52,27 +51,28 @@ class HindentFormatEditsProvider implements
   }
 
   provideDocumentFormattingEdits(
-      document: vscode.TextDocument, options: vscode.FormattingOptions,
-      token: vscode.CancellationToken):
-      vscode.TextEdit[]|Thenable<vscode.TextEdit[]> {
+    document: vscode.TextDocument, options: vscode.FormattingOptions,
+    token: vscode.CancellationToken):
+    vscode.TextEdit[] | Thenable<vscode.TextEdit[]> {
     let formatted = this.formatHindent(document.getText());
-    if (formatted != '') {
+    if (formatted !== '') {
       let range = document.validateRange(new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE));
       return [vscode.TextEdit.replace(range, formatted)];
-    }
-    else
+    } else {
       return [];
+    }
   }
 
   provideDocumentRangeFormattingEdits(
-      document: vscode.TextDocument, range: vscode.Range,
-      options: vscode.FormattingOptions, token: vscode.CancellationToken):
-      vscode.TextEdit[]|Thenable<vscode.TextEdit[]> {
+    document: vscode.TextDocument, range: vscode.Range,
+    options: vscode.FormattingOptions, token: vscode.CancellationToken):
+    vscode.TextEdit[] | Thenable<vscode.TextEdit[]> {
     let formatted = this.formatHindent(document.getText(range));
-    if (formatted != '')
+    if (formatted !== '') {
       return [vscode.TextEdit.replace(range, formatted)];
-    else
+    } else {
       return [];
+    }
   }
 }
 
@@ -81,13 +81,13 @@ export function activate(context: vscode.ExtensionContext) {
   let hindentFormatProvider = new HindentFormatEditsProvider();
 
   vscode.languages.registerDocumentFormattingEditProvider(
-      'haskell', hindentFormatProvider);
+    {scheme: '', language: 'haskell'}, hindentFormatProvider);
   vscode.languages.registerDocumentRangeFormattingEditProvider(
-      'haskell', hindentFormatProvider);
+    {scheme: '', language: 'haskell'}, hindentFormatProvider);
 
-  vscode.workspace.onDidChangeConfiguration(function(event) {
+  vscode.workspace.onDidChangeConfiguration(function (event) {
     hindentFormatProvider.configure();
   });
 }
 
-export function deactivate() {}
+export function deactivate() { }
